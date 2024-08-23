755bf7422b414a04546f681d6b18fa7f94fe876a
pundirakash <pundirakash@outlook.com>
2024-08-23 16:34:58 +0530

    Fixing height of loading spinner

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const LoadingSpinner = () => (
-  <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+  <div
+    className="d-flex justify-content-center align-items-center"
+    style={{ height: '100px', width: '100%', position: 'absolute', top: 0, left: 0 }}
+  >
     <div className="spinner-border" role="status">
       <span className="visually-hidden">Loading...</span>
     </div>
