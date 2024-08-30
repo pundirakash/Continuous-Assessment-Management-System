44dafea3df17f9189bd75cb899046b7f293ec704
pundirakash <pundirakash@outlook.com>
2024-08-30 06:16:51 +0530

    Added Network Status Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import AdminDashboard from './pages/AdminDashboard';
@@ -7,13 +7,32 @@ import HodDashboard from './pages/HodDashboard';
 import PrivateRoute from './utils/PrivateRoute';
 import RoleSelection from './pages/RoleSelection';
 import Footer from './components/Footer';
+import NetworkStatusModal from './components/NetworkStatusModal'; // Import the new modal
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './App.css'
+import './App.css';
 
 function App() {
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
+
+  // Handler for network changes
+  useEffect(() => {
+    const handleOnline = () => setIsOffline(false);
+    const handleOffline = () => setIsOffline(true);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    // Clean up the event listeners on component unmount
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
   return (
     <Router>
+      <NetworkStatusModal show={isOffline} handleClose={() => setIsOffline(false)} />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route 
